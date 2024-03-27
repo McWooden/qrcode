6138e17677bdc759d115cab2fb0e33d93411ae09
McWooden <garryf7887@gmail.com>
2024-03-27 15:51:46 +0700

    push meta

--- src/components/QrCodeGenerator.jsx
+++ src/components/QrCodeGenerator.jsx
@@ -96,7 +96,7 @@ export default function QrCodeGenerator() {
                 <div>
                     <div role="alert" className="alert alert-info">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                        <span>Tunjukkan Kode-Qr ke pemindai yang disediakan.</span>
+                        <span>Tunjukkan Kode-Qr ke pemindai yang disediakan sekolah.</span>
                     </div>
                 </div>
                 <p className="break-all text-xs">{encryptString(`${account?.nama},${account?.nomor},${account?.kelas},${account?.nomorAbsen},${ip}`)}</p>
